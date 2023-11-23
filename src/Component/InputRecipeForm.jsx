6ef57e3a5c15651dd83362a7076bd28fb6982ec8
Syodai88 <116556788+Syodai88@users.noreply.github.com>
2023-11-23 16:45:44 +0900

    入力制限の実装 エラー時の表示の変更 各関数の説明コメントの追加

--- src/Component/InputRecipeForm.jsx
+++ src/Component/InputRecipeForm.jsx
@@ -1,34 +1,70 @@
-//入力フォームのコンポーネント
 import React, { useState } from 'react';
-import { TextField, Button, Box, List, ListItem  } from '@mui/material';
+import { TextField, Button, Box, List, ListItem, Typography, Grid } from '@mui/material';
 
 const InputRecipeForm = ({ onSubmit }) => {
   const [recipe, setRecipe] = useState({ name: '', ingredients: [], steps: '' });
+  const [ingredient, setIngredient] = useState('');
+  const [quantity, setQuantity] = useState('');
+  const [errors, setErrors] = useState({});
 
+  //エラーチェックの関数、真偽値を返す
+  const validateForm = () => {
+    let newErrors = {};//エラーオブジェクトの生成
+    //テキスト入力はtrimで不要なスペースの削除
+    if (!recipe.name.trim()) newErrors.name = 'レシピ名を入力してください';
+    if (recipe.ingredients.length === 0) newErrors.ingredients = '最低1つの食材を追加してください';
+    if (!recipe.steps.trim()) newErrors.steps = '手順を入力してください';
+
+    setErrors(newErrors);
+    return Object.keys(newErrors).length === 0;//エラーがなければlengthは0
+  };
+
+  //レシピ名と手順のイベントハンドラ、フィールドのnameで確認して対応箇所に適応
   const handleChange = (e) => {
     setRecipe({ ...recipe, [e.target.name]: e.target.value });
+    setErrors({ ...errors, [e.target.name]: '' });
   };
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    onSubmit(recipe);
-  };
-
-  const [ingredient, setIngredient]=useState("");
+  //材料のイベントハンドラ
   const handleIngredientChange = (e) => {
     setIngredient(e.target.value);
   };
 
+  //手順のイベントハンドラ
+  const handleQuantityChange = (e) => {
+    setQuantity(e.target.value);
+  };
+
+  //具材：分量を追加するときのチェック
   const addIngredient = () => {
-    if (ingredient.trim() !== '') {
-      setRecipe({ ...recipe, ingredients: [...recipe.ingredients, ingredient] });
+    if (ingredient.trim() && quantity.trim()) {
+      //ingredients配列に追加、スプレット構文で新オブジェクトを生成する形
+      setRecipe({ ...recipe, ingredients: [...recipe.ingredients, `${ingredient}：${quantity}`] });
+      //入力欄とエラーの初期化
       setIngredient('');
+      setQuantity('');
+      setErrors({ ...errors, ingredients: '' });
+    } else {//どちらかが空欄の時の処理
+      setErrors({ ...errors, ingredients: '具材と分量を両方入力してください' });
+    }
+  };
+
+  //提出
+  const handleSubmit = (e) => {
+    e.preventDefault();//送信によるページのリロード防止
+    if (validateForm()) {
+      onSubmit(recipe);
     }
   };
 
   return (
+    //html要素のform指定
     <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
+      {/*レシピ名の入力欄*/}
       <TextField
+        //!!は強制的に真偽値に変換する
+        error={!!errors.name}//エラーに何もなければfalseの否定でtrue
+        helperText={errors.name}
         margin="normal"
         required
         fullWidth
@@ -36,30 +72,48 @@ const InputRecipeForm = ({ onSubmit }) => {
         name="name"
         onChange={handleChange}
       />
-      <TextField
-        margin="normal"
-        fullWidth
-        label="食材"
-        name="ingredient"
-        value={ingredient}
-        onChange={handleIngredientChange}
-      />
+      {/*Gridで具材と材料の入力欄を横並びに*/}
+      <Grid container spacing={2}>
+        <Grid item xs={6}>
+          <TextField
+            margin="normal"
+            fullWidth
+            label="具材"
+            name="ingredient"
+            value={ingredient}
+            onChange={handleIngredientChange}
+          />
+        </Grid>
+        <Grid item xs={6}>
+          <TextField
+            margin="normal"
+            fullWidth
+            label="分量"
+            name="quantity"
+            value={quantity}
+            onChange={handleQuantityChange}
+          />
+        </Grid>
+      </Grid>
       <Button onClick={addIngredient} variant="contained" sx={{ mt: 1, mb: 2 }}>
         食材を追加
       </Button>
+      {errors.ingredients && <Typography color="error">{errors.ingredients}</Typography>}
       <List>
-        {recipe.ingredients.map((ing, index) => (
-          <ListItem key={index}>{ing}</ListItem>
+        {recipe.ingredients.map((item, index) => (
+          <ListItem key={index}>{item}</ListItem>
         ))}
       </List>
       <TextField
+        error={!!errors.steps}
+        helperText={errors.steps}
         margin="normal"
         required
         fullWidth
-        label="手順"
+        label="調理方法"
         name="steps"
         multiline
-        rows={4}
+        rows={8}
         onChange={handleChange}
       />
       <Button type="submit" fullWidth variant="contained" sx={{ mt: 3, mb: 2 }}>
