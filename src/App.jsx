7b11ef843bff1b8fbfb0f60bd2e7c6dca2928c3c
Syodai88 <116556788+Syodai88@users.noreply.github.com>
2023-12-11 00:12:40 +0900

    change sampleRecipes add useEffect

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,21 @@
-import React,{useState} from 'react';
+import React,{useState,useEffect} from 'react';
 import Header from './Component/Header';
 import InputRecipeForm from './Component/InputRecipeForm';
 import RecipeCard from './Component/RecipeCard';
 import NewRecipeDetails from './Component/NewRecipeDetails';
 import Grid from '@mui/material/Grid';
 import NewRecipeImage from './stable-diffusion-v1-5.jpeg'
+import axios from 'axios';
 
 const App = () => {
   const [recipeData, setRecipeData] = useState(null);
   const [similarityData, setSimilarityData] = useState(null);
   console.log(similarityData)
+  const [sampleRecipes, setSampleRecipes] = useState([
+    { name: 'レシピ1' },
+    { name: 'レシピ2' },
+    { name: 'レシピ3' }
+  ]);
 
   //InputRecipeFormのonSubmitハンドラ
   const handleRecipeSubmit = (data) => {
@@ -18,13 +24,22 @@ const App = () => {
     console.log(similarityData)
   };
 
-  //仮のレシピデータ
-  const sampleRecipes = [
-    { name: 'レシピ1' },
-    { name: 'レシピ2' },
-    { name: 'レシピ3' }
-    // ここに他のレシピを追加できます
-  ];
+  useEffect(() => {
+    //データの有無と空配列ではないか
+    if (similarityData && similarityData.length > 0) {
+      //similarityDataからidのみを抽出
+      const ids = similarityData.map(id => id[0]);
+      
+      axios.post('/show_similarities', ids)
+        .then(response => {
+          const updatedRecipes = response.data.map(name => ({ name }));
+          setSampleRecipes(updatedRecipes);
+        })
+        .catch(error => console.error('Error:', error));
+    }
+  }, [similarityData]);
+
+  
 
   //仮のオリジナルレシピデータと画像URL
   const originalRecipe = {
