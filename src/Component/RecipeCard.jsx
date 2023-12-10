705883984eda66583b9779c6558fdb25cb7ca7e1
Syodai88 <116556788+Syodai88@users.noreply.github.com>
2023-12-11 02:30:30 +0900

    add modal

--- src/Component/RecipeCard.jsx
+++ src/Component/RecipeCard.jsx
@@ -1,17 +1,39 @@
-//類似度が高い料理を表示するコンポーネント
-import React from 'react';
-import { Card, CardContent, Typography } from '@mui/material';
+import React, { useState } from 'react';
+import { Card, CardContent, Typography, Button, Modal, Box } from '@mui/material';
+import RecipeModal from './RecipeModal'; // モーダルのコンポーネントをインポート
 
 const RecipeCard = ({ recipe }) => {
+  const [modalOpen, setModalOpen] = useState(false);
+
+  const handleOpenModal = () => {
+    setModalOpen(true);
+  };
+
+  const handleCloseModal = () => {
+    setModalOpen(false);
+  };
+
   return (
     <Card sx={{ minWidth: 275, margin: 2 }}>
       <CardContent>
         <Typography variant="h5" component="div">
           {recipe.name}
         </Typography>
+        
+        {recipe.id && (
+          <Button variant="outlined" onClick={handleOpenModal}>
+            作り方を見る！
+          </Button>
+        )}
+        
       </CardContent>
+      <RecipeModal
+        open={modalOpen}
+        handleClose={handleCloseModal}
+        recipeId={recipe.id}
+      />
     </Card>
   );
-}
+};
 
 export default RecipeCard;
