b4be330eee6c553e8bad9e5640e2e85c2357b101
Syodai88 <116556788+Syodai88@users.noreply.github.com>
2023-12-11 10:24:36 +0900

    add backend to get recipe details

--- src/Component/RecipeModal.jsx
+++ src/Component/RecipeModal.jsx
@@ -1,7 +1,31 @@
-import React from 'react';
+import React,{useState,useEffect} from 'react';
 import { Modal, Box, Typography } from '@mui/material';
 
 const RecipeModal = ({ open, handleClose, recipeId }) => {
+  //表示時に調理方法を１ステップごとに番号付で表示したいので配列
+  //group7_data内のstepsも配列なのでアクセスはこの方針
+  const [recipeDetails, setRecipeDetails]=useState({
+    name:"",
+    ingridients:[],
+    steps:[]
+  });
+
+  const fetchRecipeDetails=async()=>{
+    try {
+      const response = await axios.post('/fetch_recipe_details', recipeId);
+      console.log("fetchData_recipe_details"+response.data);
+      setRecipeDetails(response.data);
+    } catch (error) {
+      console.error('Error:', error);
+    }
+  };
+  //openかidの更新時に実行
+  useEffect(()=>{
+    if(open){
+      fetchRecipeDetails();
+    }
+  },[open,recipeId]);
+
   return (
     <Modal
       open={open}
