ca392c3d1dc90f3057c99f7cc031a1bae5cf948b
mfyu127 <151510434+mfyu127@users.noreply.github.com>
2023-12-11 15:10:44 +0900

    ヘッダーの中央表示と色

--- src/Component/Header.jsx
+++ src/Component/Header.jsx
@@ -1,15 +1,27 @@
 //ヘッダーのコンポーネト
 import React from 'react';
 import { AppBar, Toolbar, Typography } from '@mui/material';
+import { styled } from '@mui/system';
+
+const HeaderContainer = styled('div')({
+  display: 'flex',
+  justifyContent: 'center',
+  alignItems: 'center',
+  height: '60px', // Set the height of your header
+  background: 'orange', // Adjust to your header background color
+  color: theme => theme.palette.common.white, // Adjust to your header text color
+});
 
 const Header = () => {
   return (
     <AppBar position="static">
-      <Toolbar>
-        <Typography variant="h6">
-          レシピアプリ
-        </Typography>
-      </Toolbar>
+      <HeaderContainer>
+        <Toolbar>
+          <Typography variant="h6">
+            レシピアプリ
+          </Typography>
+        </Toolbar>
+      </HeaderContainer>
     </AppBar>
   );
 }
