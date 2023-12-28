a75005adbb37551c0634033154d44a6f3eddcc0d
Syodai88 <116556788+Syodai88@users.noreply.github.com>
2023-12-29 01:35:27 +0900

    レシピアプリの表示に加えてログイン後にユーザ情報を表示するアイコンやページ遷移のボタンを追加 それらの追加情報はログイン後以外は表示されないように調整

--- src/Component/Header.jsx
+++ src/Component/Header.jsx
@@ -1,8 +1,10 @@
 //ヘッダーのコンポーネト
-import React from 'react';
-import { AppBar, Toolbar, Typography } from '@mui/material';
+import React,{useState} from 'react';
+import { AppBar, Toolbar, Typography, Button, Modal, Box} from '@mui/material';
 import { styled } from '@mui/system';
 import HeaderImage from '../picture/headerlogo.png';
+import AccountCircleIcon from '@mui/icons-material/AccountCircle';
+import { useNavigate } from 'react-router-dom';
 
 const HeaderContainer = styled('div')({
   display: 'flex',
@@ -12,18 +14,109 @@ const HeaderContainer = styled('div')({
   background: 'orange', // Adjust to your header background color
   color: 'black', // Adjust to your header text color
   padding: '0 20px',
+  position: 'relative',
 });
+const Header = ({userId}) => {
+  const [modalOpen, setModalOpen] = useState(false);
+  const [logoutConfirmOpen, setLogoutConfirmOpen] = useState(false);
+  let navigate = useNavigate();
 
-const Header = () => {
+   //ユーザーモーダルの表示/非表示
+  const handleModalOpen = () => {
+    setModalOpen(true);
+  };
+  const handleModalClose = () => {
+    setModalOpen(false);
+  };
+  const handleLogout = () => {
+    //ローカルストレージからログイン情報を削除
+    localStorage.removeItem('isLoggedIn');
+    localStorage.removeItem('userId');
+    setLogoutConfirmOpen(false);
+    navigate('/login');
+    //ログインページにリダイレクト
+  };
+
+  //確認モーダルの表示/非表示
+  const openLogoutConfirm = () => {
+    setModalOpen(false);
+    setLogoutConfirmOpen(true);
+  };
+  const closeLogoutConfirm = () => {
+    setLogoutConfirmOpen(false);
+  };
+
+
+
   return (
     <AppBar position="sticky">
       <HeaderContainer>
-        <Toolbar>
-          <img src={HeaderImage} alt="Logo" style={{ height: '40px', marginRight: '10px' }} />
-          <Typography variant="h6" fontFamily="Courier New, monospace">
-            レシピアプリ
-          </Typography>
+        <Toolbar sx={{ justifyContent: 'center', width: '100%' }}>
+          {/*ロゴとテキストを中央に配置*/}
+          <Box sx={{ display: 'flex', alignItems: 'center' }}>
+            <img src={HeaderImage} alt="Logo" style={{ height: '40px', marginRight: '10px' }} />
+            <Typography variant="h6" component="div" sx={{ flexGrow: 1, fontFamily: 'Courier New, monospace' }}>
+              レシピアプリ
+            </Typography>
+          </Box>
+          {/* 右側のボタン */}
+          {userId &&(
+            <Box sx={{ position: 'absolute', right: 0 }}>
+              <Button color="inherit" onClick={() => navigate('/')}>Home</Button>
+              <Button color="inherit">Record</Button>
+              <Button color="inherit" onClick={handleModalOpen}>
+                <AccountCircleIcon />
+              </Button>
+            </Box>   
+          )}
         </Toolbar>
+         {/*ユーザーモーダル */}
+        <Modal open={modalOpen} onClose={handleModalClose}>
+          <Box
+            sx={{
+              position: 'absolute',
+              top: '50%',
+              left: '50%',
+              transform: 'translate(-50%, -50%)',
+              bgcolor: 'white',
+              p: 3,
+              minWidth: '300px',
+              maxWidth: '80%',
+              borderRadius: '8px',
+              boxShadow: 24,
+            }}
+          >
+            <Typography variant="h6">ユーザ名</Typography>
+            <Button variant="outlined" color="primary" onClick={openLogoutConfirm}>
+              ログアウト
+            </Button>
+          </Box>
+        </Modal>
+        {/*ログアウト確認用のモーダル */}
+        <Modal open={logoutConfirmOpen} onClose={closeLogoutConfirm}>
+          <Box
+            sx={{
+              position: 'absolute',
+              top: '50%',
+              left: '50%',
+              transform: 'translate(-50%, -50%)',
+              bgcolor: 'white',
+              p: 3,
+              minWidth: '300px',
+              maxWidth: '80%',
+              borderRadius: '8px',
+              boxShadow: 24,
+            }}
+          >
+            <Typography variant="h6">本当にログアウトしますか？</Typography>
+            <Button variant="contained" color="error" onClick={handleLogout}>
+              はい
+            </Button>
+            <Button variant="outlined" onClick={closeLogoutConfirm}>
+              いいえ
+            </Button>
+          </Box>
+        </Modal>
       </HeaderContainer>
     </AppBar>
   );
