c793a9e28110e1a7937f160727f71d1b6a28f60a
Syodai88 <116556788+Syodai88@users.noreply.github.com>
2023-12-29 22:49:31 +0900

    パスワードの入力欄に表示、非表示の切り替えボタンを配置 パスワードは半角英数字に入力を制限、それ以外は入力できないようにしている。

--- src/Component/LoginForm.jsx
+++ src/Component/LoginForm.jsx
@@ -1,11 +1,16 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import { Box, TextField, Button, Typography } from '@mui/material';
+import { Box, TextField, Button, Typography, IconButton} from '@mui/material';
 import axios from 'axios';
+import VisibilityIcon from "@mui/icons-material/Visibility";
+import VisibilityOffIcon from "@mui/icons-material/VisibilityOff";
 
 const LoginForm = ({ onLogin }) => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const [error, setError]=useState("");
+  const [showPassword, setShowPassword] = useState(true);
+  const [passwordType, setPasswordType] = useState("password");
   let navigate = useNavigate();
 
   const handleLoginCheck = async () => {
@@ -26,17 +31,33 @@ const LoginForm = ({ onLogin }) => {
     } catch (error) {
       if (error.response && error.response.status === 401) {
         console.error("パスワードが違います:",error.response.data.message);
+        setError("パスワードが違います");
       } else if(error.response && error.response.status === 404) {
         console.error("ユーザが見つかりません",error.response.data.message);
+        setError("ユーザが見つかりません");
       } else{
         console.error("An error occurred:", error);
+        setError("An error occurred: " + error.message);
       }
+    }
+  };
+  const handlePasswordChange = (e) => {
+    const newValue = e.target.value;
+    const isAlphanumeric = /^[a-zA-Z0-9]*$/;//入力を半角英数字に限定
+
+    if (isAlphanumeric.test(newValue) || newValue === "") {
+      setPassword(newValue);
     }
   };
 
   const handleSubmit = async (event) => {
     event.preventDefault();
+    setError("");
     handleLoginCheck();
+  };
+  const togglePasswordVisibility = () => {
+    setShowPassword(!showPassword); // パスワード表示/非表示を切り替え
+    setPasswordType(showPassword ? 'text' : 'password');
   };
 
   return (
@@ -49,14 +70,28 @@ const LoginForm = ({ onLogin }) => {
             value={email}
             onChange={(e) => setEmail(e.target.value)}
             margin="normal"
+            error={error === "ユーザが見つかりません"}
             />
             <TextField
             label="パスワード"
-            type="password"
+            type={passwordType}
             value={password}
-            onChange={(e) => setPassword(e.target.value)}
+            onChange={handlePasswordChange}
             margin="normal"
+            error={error === "パスワードが違います"}
+            InputProps={{
+              endAdornment: (
+                <IconButton onClick={togglePasswordVisibility}>
+                  {showPassword ? <VisibilityOffIcon /> : <VisibilityIcon />}
+                </IconButton>
+              ),
+            }}
             />
+            {error && (
+              <Typography style={{ color: 'red', marginTop: 10 }}>
+                {error}
+              </Typography>
+            )}
             <Button type="submit" variant="contained" color="primary" style={{ marginTop: 20 }}>
             ログイン
             </Button>
