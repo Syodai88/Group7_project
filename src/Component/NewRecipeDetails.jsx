b0e431503055ff2d5ed2f0c01f863faaecf5c546
mfyu127 <151510434+mfyu127@users.noreply.github.com>
2024-01-15 16:43:54 +0900

    画像生成に失敗した時の身代わりパンダ

--- src/Component/NewRecipeDetails.jsx
+++ src/Component/NewRecipeDetails.jsx
@@ -2,12 +2,13 @@
 import React from 'react';
 import {Box, Paper } from '@mui/material';
 import NewRecipeModal from './NewRecipeModal';
-//imgのaltに画像を生成できなかった場合の画像が欲しい、できれば謝ってる感じのパンダ
+import makingfailImage from './../picture/makingfail.png';
+
 const NewRecipeDetails = ({ recipe, imageUrl, open, onClose, onSave }) => {
   return (
     <Box sx={{ p: 2 }}>
       <Paper elevation={3} sx={{ mt: 2 }}>
-        <img src={imageUrl} alt={recipe} style={{ width: '100%' }} />
+        <img src={imageUrl} alt={makingfailImage} style={{ width: '100%' }} />
       </Paper>
       <NewRecipeModal open={open} recipe={recipe} onClose={onClose} onSave={onSave} />
     </Box>
