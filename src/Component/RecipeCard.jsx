de339d7e76cc6263cf69d34643be33fb8308957c
Syodai88 <116556788+Syodai88@users.noreply.github.com>
2024-01-22 13:09:34 +0900

    類似度別の評価を表すマークを追加

--- src/Component/RecipeCard.jsx
+++ src/Component/RecipeCard.jsx
@@ -1,8 +1,11 @@
 import React, { useState } from 'react';
 import { Card, CardContent, Typography, Button} from '@mui/material';
 import RecipeModal from './RecipeModal'; 
+import CheckCircleIcon from '@mui/icons-material/CheckCircle';
+import WarningIcon from '@mui/icons-material/Warning';
+import DangerousIcon from '@mui/icons-material/Dangerous';
 
-const RecipeCard = ({ recipe, onRecipeSelect}) => {
+const RecipeCard = ({ recipe, index, onRecipeSelect}) => {
   const [modalOpen, setModalOpen] = useState(false);
   const handleOpenModal = () => {
     setModalOpen(true);
@@ -10,13 +13,27 @@ const RecipeCard = ({ recipe, onRecipeSelect}) => {
   const handleCloseModal = () => {
     setModalOpen(false);
   };
+
+  const getIcon = (index,id) => {
+    if(id){
+      if (index % 6 === 0 || index % 6 === 1) {
+        return <CheckCircleIcon style={{ color: '#00cc66' }} />;
+      } else if (index % 6 === 2 || index % 6 === 3) {
+        return <WarningIcon style={{ color: '#ffcc00' }} />;
+      } else {
+        return <DangerousIcon style={{ color: '#b11d2c' }} />;
+      }
+    }
+  };
+
   
 
   return (
-    <Card sx={{ minWidth: 275, margin: 2 }}>
+    <Card sx={{ minWidth: 275, margin: 2, backgroundColor:"#efefef"}}>
       <CardContent>
         <Typography variant="h5" component="div">
-          {recipe.name}
+          {getIcon(index,recipe.id)}
+          <span style={{ marginLeft: '10px' }}>{recipe.name}</span>
         </Typography>
         
         {recipe.id && (
